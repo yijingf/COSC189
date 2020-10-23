1483a0d3b254bf5065865951bd4616ea0f68e90c
K_HOLMES_ <K_HOLMES_@outlook.com>
2020-10-23 14:27:53 -0400

    Trainable Model

--- main_spatial.py
+++ main_spatial.py
@@ -1,21 +1,102 @@
+import resnet as ResNet
+from utils import SpatialDataset, calculate_accuracy, AverageMeter
+
 import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
 import numpy as np
 import os
-import resnet as ResNet
+import argparse
 
-device = "cpu" if torch.cuda.is_available() else "cuda:0"
+seed = 42
+torch.manual_seed(seed)
+np.random.seed(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+device = "cpu" if not torch.cuda.is_available() else "cuda:0"
 
 
-def load_pretrained_model(model_path):
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--interval", type=int, default=3)
+    parser.add_argument("--batch_size", type=int, default=64)
+    return parser.parse_args()
+
+
+def load_pretrained_model_utils(model_path):
     ckpt = torch.load(model_path)
     resnet = ResNet.generate_model(model_depth=18, n_classes=700)
     resnet.load_state_dict(ckpt['state_dict'])
     return resnet
 
 
+def eval_model(model, data):
+    val_accuracies = AverageMeter()
+    with torch.no_grad():
+        for i, (input_data, labels) in enumerate(data):
+            input_data = input_data.to(device)
+            labels = labels.flatten().to(device)
+
+            outputs = model(input_data)
+            acc = calculate_accuracy(outputs, labels)
+
+            val_accuracies.update(acc, input_data.shape[0])
+    return val_accuracies
+
+
+def train(model, train_data, val_data, criterion, optimizer, tb_writer, ckpt_saved_path, save_name):
+    best_acc = -float('inf')
+
+    for ind_epo in range(1, 50):
+        losses = AverageMeter()
+        accuracies = AverageMeter()
+        val_accuracies = AverageMeter()
+        model.train()
+        for i, (input_data, labels) in enumerate(train_data):
+            input_data = input_data.to(device)
+            labels = labels.flatten().to(device)
+
+            outputs = model(input_data)
+            loss = criterion(outputs, labels)
+            acc = calculate_accuracy(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            losses.update(loss.cpu().item(), input_data.shape[0])
+            accuracies.update(acc, input_data.shape[0])
+            if i % (len(train_data) // 10) == 0:
+                print(f'Epoch: [{ind_epo}][{i + 1}/{len(train_data)}]\t' +
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Acc {acc.val:.3f} ({acc.avg:.3f})'.format(loss=losses,
+                                                                 acc=accuracies))
+        model.eval()
+        val_accuracies = eval_model(model, val_data)
+
+        print(f'Epoch: {ind_epo} \t Acc:{round(val_accuracies.avg, 3)}')
+        if best_acc < val_accuracies.avg:
+            best_acc = val_accuracies.avg
+            torch.save({'model':model.state_dict(),
+                        'epoch':ind_epo,
+                        'optimizer':optimizer},
+                       os.path.join(ckpt_saved_path, f"epoch_{ind_epo}_{save_name}.pt"))
+
+        tb_writer.add_scalar('train/loss', losses.avg, ind_epo)
+        tb_writer.add_scalar('train/acc', accuracies.avg, ind_epo)
+        tb_writer.add_scalar('val/acc', val_accuracies.avg, ind_epo)
+
+
+def test(model, data):
+    val_accuracies = eval_model(model, data)
+    print(f'Test Acc:{round(val_accuracies.avg, 3)}')
+
 def main():
     pretrained_path = "./models/r3d18_K_200ep.pth"
-    resnet = load_pretrained_model(pretrained_path)
+
+    resnet = load_pretrained_model_utils(pretrained_path)
 
     # random_data = torch.rand((1, 3, 10, 256, 256))
     # out = resnet(random_data)
@@ -29,7 +110,32 @@ def main():
                                    padding=(3, 3, 3),
                                    bias=False)
     resnet.fc = torch.nn.Linear(resnet.fc_in, 5)
+    resnet.to(device)
+
+    optimizer = torch.optim.SGD(resnet.parameters(),
+                                lr=1e-3,
+                                momentum=0.9,
+                                weight_decay=1e-3)
+
+    interval_size = args.interval
+    train_data_loader = DataLoader(SpatialDataset(mode='train', max_interval_size=interval_size),
+                                   batch_size=args.batch_size, shuffle=True)
+    val_data_loader = DataLoader(SpatialDataset(mode='val', max_interval_size=interval_size),
+                                 batch_size=args.batch_size, shuffle=False)
+    tb_writer = SummaryWriter(log_dir="./log", filename_suffix=f'_interval_{interval_size}')
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+
+    train(resnet,
+          train_data=train_data_loader,
+          val_data=val_data_loader,
+          criterion=criterion,
+          optimizer=optimizer,
+          tb_writer=tb_writer,
+          ckpt_saved_path=os.path.join('models', 'spatial_model'),
+          save_name=f'_interval_{interval_size}'
+          )
 
 
 if __name__ == '__main__':
+    args = get_args()
     main()
---
+++ utils.py
@@ -0,0 +1,79 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import os, random
+
+random.seed(42)
+np.random.seed(42)
+
+
+class SpatialDataset(Dataset):
+    def __init__(self, root_path='./data/spatial', mode='train', max_interval_size=3):
+        '''
+        :param root_path: path for saving pre-processed data, train, eval, test
+        :return:
+        '''
+        self.dataset_path = os.path.join(root_path, mode)
+        self.label2int = {'ambient':0, 'symphonic':1, 'metal':2, 'rocknroll':3, 'country':4}
+        self.item_list = sorted(os.listdir(self.dataset_path))
+        self.max_interval_size = max_interval_size
+        print(self.dataset_path, len(self.item_list))
+
+    def __len__(self):
+        return len(self.item_list)
+
+    def __getitem__(self, index):
+        item_name = self.item_list[index]
+        data_label_dict = torch.load(os.path.join(self.dataset_path, item_name))
+        data, label = data_label_dict['img'], self.label2int[data_label_dict['label']]
+
+        # pad information or split, [z, time, x, y]
+        time_len = data.shape[1]
+        if time_len < self.max_interval_size:
+            pad = np.zeros([data.shape[0], self.max_interval_size-time_len] + list(data.shape[2:]))
+            data = np.concatenate([data, pad], axis=1)
+        else:
+            rand_idx = [i for i in range(time_len)]
+            random.shuffle(rand_idx)
+            rand_idx = rand_idx[:self.max_interval_size]
+            data = data[:, rand_idx, :, :]
+        data = data.astype(np.float32)
+        return data, np.array([label], dtype=np.int)
+
+
+class AverageMeter():
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def calculate_accuracy(outputs, targets):
+    with torch.no_grad():
+        batch_size = targets.shape[0]
+        _, pred = outputs.topk(1, 1, largest=True, sorted=True)
+        pred = pred.t()
+        correct = pred.eq(targets.view(1, -1))
+        n_correct_elems = correct.float().sum().item()
+        return n_correct_elems / batch_size
+
+
+if __name__ == '__main__':
+    s_dataset = SpatialDataset(mode='train')
+    data_loader = DataLoader(s_dataset, batch_size=4, shuffle=True)
+    print(data_loader)
+    for each in data_loader:
+        imgs, labels = each
+        print(imgs.shape, labels.shape)
